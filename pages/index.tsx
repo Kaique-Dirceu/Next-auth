1f5c025a29fedec37704539f841e6ef2c103eb4b
Kaique Dirceu <dirceukaique@gmail.com>
2021-07-14 12:13:45 -0300

    add framer-motion

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div className="bg-white dark:bg-gray-800">
+      <p className="text-gray-600 dark:text-gray-300">
+        Lorem ipsum...
+      </p>
+    </div>
+  )
+}
